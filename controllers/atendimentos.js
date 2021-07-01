b76cdc4a16c5a44e36d277ebb882632940e8c93d
Jeff Slywitch <jeferson.slywitch@gmail.com>
2021-07-01 18:01:49 -0300

    terminando capitulo 3

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimentos = require ('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET.'))
 
     app.post('/atendimentos', (req, res) =>  {
-        console.log(req.body)
+        const atendimento = req.body
+
+        Atendimentos.adiciona(atendimento)
         res.send('Você está na rota de atendimentos e está realizando um POST .')
     })
 }
