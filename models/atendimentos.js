3c449bc6daa9661239f1d44fec3ffafa96759cbf
Jeff Slywitch <jeferson.slywitch@gmail.com>
2021-07-04 23:00:47 -0300

    andamento do capitulo 4

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require ('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
     adiciona (atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (erro, resultado) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultado) => {
             if (erro) {
                 console.log(erro)
             } else {
